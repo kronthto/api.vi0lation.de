82e2765f296749c4431543c02ec38ab4fa1b2fa8
Tobias Kronthaler <kronthto@gmail.com>
2019-05-02 12:20:50 +0200

    Try to improve memory usage of omi parsing

--- app/Services/OmiService.php
+++ app/Services/OmiService.php
@@ -8,6 +8,13 @@ use Kronthto\AOEncrypt\HexXorer;
 class OmiService
 {
     public const NS_CR = 'CR';
+    protected const KEEP_CATS = [
+        'monsters',
+        'rareitems',
+        'mysteryitemdrop',
+        'item',
+        'MIXING_INFO',
+    ];
 
     /** @var string */
     protected $path;
@@ -25,18 +32,31 @@ class OmiService
 
     protected function loadData(): OmiReadResult
     {
-        return \Cache::remember(sprintf('omi.tex_'.$this->ns), 180, function(): OmiReadResult {
+        return igbinary_unserialize(\Cache::remember(sprintf('omi.tex_'.$this->ns), 360, function(): OmiReadResult {
             $omiReader = new \Kronthto\AOArchive\Omi\Reader\OmiReader();
 
             $readOmi = $omiReader->parse(file_get_contents($this->path));
+
+            // remove null vals / uninterssant
+            foreach ($readOmi as $key => &$omiNsValue) {
+                if (!in_array($key, static::KEEP_CATS)) {
+                    unset($readOmi->{$key});
+                }
+            }
 
             if ($this->ns === self::NS_CR) {
                 $xor = new HexXorer(config('cr.xorKeyHex'));
                 $readOmi = $readOmi->map([$xor, 'doXor']);
             }
 
-            return $readOmi->parseToEntities();
-        });
+            $serialized = igbinary_serialize($readOmi->parseToEntities());
+
+            if (\strlen($serialized) < 10000) {
+                throw new \RuntimeException('Omi parsing / serialization error - too short');
+            }
+
+            return $serialized;
+        }));
     }
 
     public function getData(): OmiReadResult
