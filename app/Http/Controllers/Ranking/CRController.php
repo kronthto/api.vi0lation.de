4b7bc7e7cc8446e76ab7f0fe07c2fb61d02e144e
Tobias Kronthaler <kronthto@gmail.com>
2019-08-25 17:37:08 +0200

    :bug: Fix from/to swap error 🤦‍

--- app/Http/Controllers/Ranking/CRController.php
+++ app/Http/Controllers/Ranking/CRController.php
@@ -235,6 +235,6 @@ class CRController extends Controller
 
         $format = 'Y-m-d H:i';
 
-        return redirect('https://beta.vi0lation.de/ranking/chromerivals/topkillsinterval?'.http_build_query(['from' => Carbon::parse($latest)->format($format), 'to' => Carbon::parse($closest24hAgo)->format($format)], null, '&', PHP_QUERY_RFC3986));
+        return redirect('https://beta.vi0lation.de/ranking/chromerivals/topkillsinterval?'.http_build_query(['from' => Carbon::parse($closest24hAgo)->format($format), 'to' => Carbon::parse($latest)->format($format)], null, '&', PHP_QUERY_RFC3986));
     }
 }
